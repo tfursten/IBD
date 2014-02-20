015bad477ddd192f0bc735847cc70e15a50b84c3
tfursten <tfursten@asu.edu>
2014-02-20 11:44:43 -0700

    Make git ignore init.txt, outfile.txt and build files. Add Main.h

---
+++ Main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_INCLUDED
+#define MAIN_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <unistd.h>
+#include <boost/program_options.hpp>
+#include "xorshift64.h"
+#include "rexp.h"
+
+
+
+
+
+
+#endif // MAIN_INCLUDED
